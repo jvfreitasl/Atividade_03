1010870b4aaaba29a76617604412f0802bb0f16c
João Victor de F. Lima <jvdfreitas@gmail.com>
2024-09-23 14:44:32 -0300

    Primeiro commit

---
+++ atividade03/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Atividade 03 - BootCamp Front-End; by: iTalents</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
